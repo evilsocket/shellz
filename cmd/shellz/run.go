77b689d519b5ac269f13014fadf0178955f1786f
evilsocket <evilsocket@gmail.com>
2018-10-07 13:24:28 +0200

    misc: small fix or general refactoring i did not bother commenting

--- cmd/shellz/run.go
+++ cmd/shellz/run.go
@@ -15,42 +15,48 @@ import (
 )
 
 var (
-	outLock = sync.Mutex{}
-	wq      = queue.New(-1, cmdWorker)
+	toOutputLock = sync.Mutex{}
+	wq           = queue.New(-1, cmdWorker)
 )
 
-func processOutput(out []byte, shell models.Shell) string {
-	outLock.Lock()
-	defer outLock.Unlock()
+func toOutputFilename(shell models.Shell) string {
+	buff := bytes.Buffer{}
+	if tmpl, err := template.New("filename").Parse(toOutput); err != nil {
+		log.Fatal("error while parsing '%s': %s", toOutput, err)
+	} else if err := tmpl.Execute(&buff, shell); err != nil {
+		log.Fatal("error while running '%s' on shell %s: %s", toOutput, shell.Name, err)
+	}
+	return buff.String()
+}
 
+func toOutputFile(shell models.Shell, out []byte) (outs string) {
+	toOutputLock.Lock()
+	defer toOutputLock.Unlock()
+
+	fileName := toOutputFilename(shell)
+	if file, err := os.OpenFile(fileName, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644); err != nil {
+		outs = fmt.Sprintf(" > error while saving to %s: %s", fileName, err)
+	} else {
+		defer file.Close()
+		size := len(out)
+		if wrote, err := file.Write(out); err != nil {
+			outs = fmt.Sprintf(" > error while saving to %s: %s", fileName, err)
+		} else if wrote != size {
+			outs = fmt.Sprintf(" > error while saving to %s: size is %d, wrote %d", fileName, size, wrote)
+		} else {
+			outs = core.Dim(fmt.Sprintf(" > %d bytes saved to %s", size, fileName))
+		}
+	}
+	return
+}
+
+func processOutput(out []byte, shell models.Shell) string {
 	outs := core.Dim(" <no output>")
 	if out != nil {
-		fileName := toOutput
-		if fileName == "" {
+		if toOutput == "" {
 			outs = fmt.Sprintf("\n\n%s\n", core.Trim(string(out)))
 		} else {
-			size := len(out)
-			buff := bytes.Buffer{}
-			if tmpl, err := template.New("filename").Parse(toOutput); err != nil {
-				panic(err)
-			} else if err := tmpl.Execute(&buff, shell); err != nil {
-				panic(err)
-			} else {
-				fileName = buff.String()
-			}
-
-			if file, err := os.OpenFile(fileName, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644); err != nil {
-				outs = fmt.Sprintf(" > error while saving to %s: %s", fileName, err)
-			} else {
-				defer file.Close()
-				if wrote, err := file.Write(out); err != nil {
-					outs = fmt.Sprintf(" > error while saving to %s: %s", fileName, err)
-				} else if wrote != size {
-					outs = fmt.Sprintf(" > error while saving to %s: size is %d, wrote %d", fileName, size, wrote)
-				} else {
-					outs = core.Dim(fmt.Sprintf(" > %d bytes saved to %s", size, fileName))
-				}
-			}
+			outs = toOutputFile(shell, out)
 		}
 	}
 	return outs
@@ -78,8 +84,8 @@ func onTestSuccess(sh models.Shell) {
 func cmdWorker(job queue.Job) {
 	start := time.Now()
 	shell := job.(models.Shell)
-	err, session := shell.NewSession(timeouts)
 
+	err, session := shell.NewSession(timeouts)
 	if err != nil {
 		if doTest {
 			onTestFail(shell, err)
@@ -99,8 +105,14 @@ func cmdWorker(job queue.Job) {
 		}
 	} else {
 		took := core.Dim(time.Since(start).String())
-		host := core.Dim(fmt.Sprintf("%s@%s", shell.Identity.Username, shell.Host))
 		outs := processOutput(out, shell)
+		host := ""
+		if shell.Identity.Username != "" {
+			host = core.Dim(fmt.Sprintf("%s@%s", shell.Identity.Username, shell.Host))
+		} else {
+			host = core.Dim(shell.Host)
+		}
+
 		if err != nil {
 			log.Error("%s (%s %s %s) > %s (%s)%s",
 				core.Bold(shell.Name),
