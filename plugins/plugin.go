fbac33ad16141e7e06a050e3c3b9b613ae789057
evilsocket <evilsocket@gmail.com>
2018-10-09 18:26:21 +0200

    refact: refactored and optimized plugins system

--- plugins/plugin.go
+++ plugins/plugin.go
@@ -22,9 +22,10 @@ type Plugin struct {
 	Path string
 
 	vm       *otto.Otto
-	cbCreate *otto.Script
-	cbExec   *otto.Script
-	cbClose  *otto.Script
+	cbCreate otto.Value
+	cbExec   otto.Value
+	cbClose  otto.Value
+	ctx      otto.Value
 }
 
 func LoadPlugin(path string, doCompile bool) (error, *Plugin) {
@@ -47,59 +48,31 @@ func LoadPlugin(path string, doCompile bool) (error, *Plugin) {
 	return nil, plugin
 }
 
-func (p *Plugin) create(sh models.Shell, timeouts core.Timeouts) (error, otto.Value) {
-	if err := p.vm.Set("sh", sh); err != nil {
-		return err, otto.UndefinedValue()
-	} else if _, err := p.vm.Run(p.cbCreate); err != nil {
-		return err, otto.UndefinedValue()
-	} else if ret, err := p.vm.Get("obj"); err != nil {
-		return err, otto.UndefinedValue()
-	} else {
-		return nil, ret
+func (p *Plugin) findCall(name string) (cb otto.Value, err error) {
+	if cb, err = p.vm.Get(name); err != nil {
+		return
+	} else if !cb.IsFunction() {
+		err = fmt.Errorf("%s is not a function", name)
 	}
+	return
 }
 
-func (p *Plugin) exec(cmd string) (error, otto.Value) {
-	if err := p.vm.Set("cmd", cmd); err != nil {
-		return err, otto.UndefinedValue()
-	} else if _, err := p.vm.Run(p.cbExec); err != nil {
-		return err, otto.UndefinedValue()
-	} else if ret, err := p.vm.Get("ret"); err != nil {
-		return err, otto.UndefinedValue()
-	} else {
-		return nil, ret
-	}
-}
-
-func (p *Plugin) compile() error {
+func (p *Plugin) compile() (err error) {
 	p.vm = otto.New()
 	// define built in functions and objects
-	if err := p.doDefines(); err != nil {
-		return err
+	if err = p.doDefines(); err != nil {
+		return
 	}
 	// run the code once in order to define all the functions
-	// and validate the syntax
-	if _, err := p.vm.Run(p.Code); err != nil {
-		return err
-	}
-	// validate and precompile callbacks
-	if err := p.compileCall(&p.cbCreate, "Create", "var obj = Create(sh)"); err != nil {
+	// and validate the syntax, then get the callbacks
+	if _, err = p.vm.Run(p.Code); err != nil {
+		return
+	} else if p.cbCreate, err = p.findCall("Create"); err != nil {
 		return fmt.Errorf("error while compiling Create callback for %s: %s", p.Path, err)
-	} else if err = p.compileCall(&p.cbExec, "Exec", "var ret = Exec(obj, cmd);"); err != nil {
+	} else if p.cbExec, err = p.findCall("Exec"); err != nil {
 		return fmt.Errorf("error while compiling Exec callback for %s: %s", p.Path, err)
-	} else if err = p.compileCall(&p.cbClose, "Close", "Close(obj);"); err != nil {
+	} else if p.cbClose, err = p.findCall("Close"); err != nil {
 		return fmt.Errorf("error while compiling Close callback for %s: %s", p.Path, err)
-	}
-	return nil
-}
-
-func (p *Plugin) compileCall(script **otto.Script, name string, call string) error {
-	if cb, err := p.vm.Get(name); err != nil {
-		return err
-	} else if !cb.IsFunction() {
-		return fmt.Errorf("%s is not a function", name)
-	} else if *script, err = p.vm.Compile("", call); err != nil {
-		return err
 	}
 	return nil
 }
@@ -113,7 +86,7 @@ func (p *Plugin) NewSession(sh models.Shell, timeouts core.Timeouts) (err error,
 	p.Lock()
 	defer p.Unlock()
 	clone = p.clone()
-	err, _ = clone.create(sh, timeouts)
+	clone.ctx, err = clone.cbCreate.Call(otto.NullValue(), sh)
 	return
 }
 
@@ -125,7 +98,7 @@ func (p *Plugin) Exec(cmd string) ([]byte, error) {
 	p.Lock()
 	defer p.Unlock()
 
-	if err, ret := p.exec(cmd); err != nil {
+	if ret, err := p.cbExec.Call(otto.NullValue(), p.ctx, cmd); err != nil {
 		return nil, err
 	} else if ret.IsNull() || ret.IsUndefined() {
 		return []byte{}, nil
@@ -141,7 +114,7 @@ func (p *Plugin) Exec(cmd string) ([]byte, error) {
 func (p *Plugin) Close() {
 	p.Lock()
 	defer p.Unlock()
-	if _, err := p.vm.Run(p.cbClose); err != nil {
+	if _, err := p.cbClose.Call(otto.NullValue(), p.ctx); err != nil {
 		log.Warning("error while running Close callback for plugin %s: %s", p.Name, err)
 	}
 }
